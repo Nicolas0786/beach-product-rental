461b9eb9184d3dc54f9f61e925b6e2af82f9b2ab
NicolasC <8083@tcesc.tc.br>
2025-08-25 20:05:59 -0300

    ajuste, usando strategy

--- src/main/java/com/salsatechnology/service/pricing/BeachTablePricingStrategy.java
+++ src/main/java/com/salsatechnology/service/pricing/BeachTablePricingStrategy.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import java.math.BigDecimal;
 
 @Component
-public class BeachITablePricingStrategy implements ProductPricingStrategy {
+public class BeachTablePricingStrategy implements ProductPricingStrategy {
 
     @Override
     public ProductType getProductType() {
